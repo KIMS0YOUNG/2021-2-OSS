15fb8715c65d8ee444669bbaf0d03af8f39c05c7
KIM SOYOUNG <80085828+KIMS0YOUNG@users.noreply.github.com>
2021-11-29 11:36:44 +0900

    Update lab2-3.sh
    주석 추가

--- src/Lab2/lab2-3.sh
+++ src/Lab2/lab2-3.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
-height2=`echo "scale=2;$2 / 100" |bc`
+height2=`echo "scale=2;$2 / 100" |bc` 
+#소수점 둘째자리까지 표현. bmi는 신장을 미터 단위로 계산하기 때문에 나누기 100
+#소수점 계산은 |bc 사용해야 함.
+
 bmi=`echo "scale=2;$1 / ($height2 * $height2)" |bc`
 
 if [ 1 -eq "$(echo "$bmi >= 18.5" |bc)" ] && [ 1 -eq "$(echo "$bmi < 23" |bc)" ];
+#bmi가 18.5이상 23미만일 때
 then
 	echo "정상체중입니다."
+	
 elif [ 1 -eq "$(echo "$bmi < 18.5" |bc)" ]
+#bmi가 18.5미만일 때
 then
 	echo "저체중입니다."
-else 
+else #이외의 경우
 	echo "과체중입니다."
 fi
 exit 0
